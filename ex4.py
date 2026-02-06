7dbabe3a30d5af776e44532bf28d569b2c81aaaa
Misha <mihailavetisan92@gmail.com>
2026-02-06 13:26:34 +0200

    .

--- ex4.py
+++ ex4.py
@@ -1,8 +1,19 @@
 last_name: str = input("What is your last name? ")
 first_name: str = input("What is your first name? ")
-
-country : str = input("What is your country? ")
+while True:
+    country : str = input("What is your country? Use not more than 2 letters ")
+    if country.isalpha() == True and len(country) < 3:
+        break
+    else:
+        print("invalid country")
 city_address : str = input("What is your city address? ")
-zipcode : str = input("What is your zipcode? ")
+while True:
+    zipcode : str = input("What is your zipcode? Use only digits and more then 3 ")
+    if zipcode.isdigit() == True and len(zipcode) > 3:
+        break
+    else:
+        print("invalid zipcode")
 print(f"FOR:{last_name.isupper()}, {first_name.islower()}")
-print(f"COUNTRY: ")
+print(f"COUNTRY:{country.isupper()} ")
+print(f"CITY ADDRESS:{city_address}")
+print(f"ZIPCODE:{zipcode}")
